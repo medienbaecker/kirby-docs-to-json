2631763ac25f0de776fd93f1b62ece79ead5e078
Thomas Günther <mail@medienbaecker.com>
2021-07-27 10:32:07 +0200

    Update 4.00

--- index.php
+++ index.php
@@ -12,6 +12,9 @@ Kirby::plugin('medienbaecker/snippet-generator', [
           function addToArray($pages, &$json) {
             $array = array();
             foreach($pages as $page) {
+
+              if(!Str::startsWith($page->title()->value(), '$')) continue;
+
               $parameters = array_column($page->parameters(), 'export');
               $body = array();
               $c = 0;
@@ -25,9 +28,9 @@ Kirby::plugin('medienbaecker/snippet-generator', [
               }
 
               $json[$page->title()->value()] = array(
-                "prefix" => '->' . $page->methodName() . '()',
-                "body" => '->' . $page->methodName() . '(' . implode($body, ", ") . ')',
-                "description" => $page->excerpt()->escape()->value(),
+                "prefix" => '->' . $page->name() . '()',
+                "body" => '->' . $page->name() . '(' . implode(", ", $body) . ')',
+                "description" => $page->intro()->escape()->value(),
                 "scope" => "php"
               );
               
@@ -35,46 +38,58 @@ Kirby::plugin('medienbaecker/snippet-generator', [
           }
 
           // Field methods
-          addToArray(page("docs/reference/templates/field-methods")->children()->visible(), $json);
-
-          // File methods
-          addToArray(page("docs/reference/objects/file")->children()->visible(), $json);
-
-          // Files methods
-          addToArray(page("docs/reference/objects/files")->children()->visible(), $json);
+          addToArray(page("docs/reference/templates/field-methods")->children()->listed(), $json);
 
           // Kirby methods
-          addToArray(page("docs/reference/objects/kirby")->children()->visible(), $json);
-
-          // Language methods
-          addToArray(page("docs/reference/objects/language")->children()->visible(), $json);
-
-          // Languages methods
-          addToArray(page("docs/reference/objects/languages")->children()->visible(), $json);
-
-          // Page methods
-          addToArray(page("docs/reference/objects/page")->children()->visible(), $json);
-
-          // Pages methods
-          addToArray(page("docs/reference/objects/pages")->children()->visible(), $json);
-
-          // Pagination methods
-          addToArray(page("docs/reference/objects/pagination")->children()->visible(), $json);
-
-          // Request methods
-          addToArray(page("docs/reference/objects/request")->children()->visible(), $json);
-
-          // Session methods
-          addToArray(page("docs/reference/objects/session")->children()->visible(), $json);
+          addToArray(page("docs/reference/objects/cms/app")->children()->listed(), $json);
 
           // Site methods
-          addToArray(page("docs/reference/objects/site")->children()->visible(), $json);
+          addToArray(page("docs/reference/objects/cms/site")->children()->listed(), $json);
+
+          // Page methods
+          addToArray(page("docs/reference/objects/cms/page")->children()->listed(), $json);
+
+          // Pages methods
+          addToArray(page("docs/reference/objects/cms/pages")->children()->listed(), $json);
+
+          // File methods
+          addToArray(page("docs/reference/objects/cms/file")->children()->listed(), $json);
+
+          // Files methods
+          addToArray(page("docs/reference/objects/cms/files")->children()->listed(), $json);
 
           // User methods
-          addToArray(page("docs/reference/objects/user")->children()->visible(), $json);
+          addToArray(page("docs/reference/objects/cms/user")->children()->listed(), $json);
 
           // Users methods
-          addToArray(page("docs/reference/objects/users")->children()->visible(), $json);
+          addToArray(page("docs/reference/objects/cms/users")->children()->listed(), $json);
+
+          // Blocks methods
+          addToArray(page("docs/reference/objects/cms/blocks")->children()->listed(), $json);
+
+          // Blocks methods
+          addToArray(page("docs/reference/objects/cms/block")->children()->listed(), $json);
+
+          // Layouts methods
+          addToArray(page("docs/reference/objects/cms/layouts")->children()->listed(), $json);
+
+          // Layout methods
+          addToArray(page("docs/reference/objects/cms/layout")->children()->listed(), $json);
+
+          // Language methods
+          addToArray(page("docs/reference/objects/cms/language")->children()->listed(), $json);
+
+          // Languages methods
+          addToArray(page("docs/reference/objects/cms/languages")->children()->listed(), $json);
+
+          // Pagination methods
+          addToArray(page("docs/reference/objects/cms/pagination")->children()->listed(), $json);
+
+          // Request methods
+          addToArray(page("docs/reference/objects/http/request")->children()->listed(), $json);
+
+          // Session methods
+          addToArray(page("docs/reference/objects/session/session-data")->children()->listed(), $json);
 
           return $json;
         }
